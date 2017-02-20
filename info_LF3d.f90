7c31f8654241695fc04e9a50cdd1dea3abcbdbae
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-02-20 20:00:43 +0700

    Update printing out of grid related variables

--- info_LF3d.f90
+++ info_LF3d.f90
@@ -1,54 +1,45 @@
 SUBROUTINE info_LF3d()
   USE m_LF3d
   IMPLICIT NONE
-  INTEGER :: N
+  INTEGER :: N, Nx, Ny, Nz
 
-  WRITE(*,*) '--------------------------'
-  WRITE(*,*) 'Basis function information'
-  WRITE(*,*) '--------------------------'
+  WRITE(*,*) '--------------------------------------------------------------'
   WRITE(*,*)
-  WRITE(*,'(1x,A,3F18.10)') 'Box size = ', LF3d_LL(:)
-  WRITE(*,'(1x,A,3I10)') 'Sampling Nx, Ny, Nz = ', LF3d_NN(:)
-  WRITE(*,'(1x,A,I10)') 'Number of points = ', LF3d_Npoints
-  WRITE(*,'(1x,A,3F18.10)') 'Grid spacing = ', LF3d_hh
-  WRITE(*,'(1x,A,F18.10)') 'dVol = ', LF3d_dVol
+  WRITE(*,*) 'Grid related variables'
+  WRITE(*,*) '^^^^^^^^^^^^^^^^^^^^^^'
+  WRITE(*,*)
+  WRITE(*,'(1x,A,3F10.5)') 'Box size            = ', LF3d_LL(:)
+  WRITE(*,'(1x,A,3F10.5)') 'Grid spacing        = ', LF3d_hh
+  WRITE(*,'(1x,A,3I5)')    'Sampling Nx, Ny, Nz = ', LF3d_NN(:)
+  WRITE(*,'(1x,A,I10)')    'Number of points    = ', LF3d_Npoints
+  WRITE(*,'(1x,A,F10.5)')  'dVol                = ', LF3d_dVol
 
   ! x-direction --------------------------------------
-  N = LF3d_NN(1)
-  WRITE(*,'(/,1x,A)') 'Some grid points in x-direction:'
-  WRITE(*,*) 1, LF3d_grid_x(1)
-  WRITE(*,*) 2, LF3d_grid_x(2)
-  WRITE(*,*) '....'
-  WRITE(*,*) N-1, LF3d_grid_x(N-1)
-  WRITE(*,*) N,   LF3d_grid_x(N)
+  Nx = LF3d_NN(1)
+  Ny = LF3d_NN(2)
+  Nz = LF3d_NN(3)
+  WRITE(*,'(/,1x,A,/)') 'Some grid points in x, y, and z directions:'
+  WRITE(*,fmt=999) 1, LF3d_grid_x(1), 1, LF3d_grid_y(1), 1, LF3d_grid_z(1)
+  WRITE(*,fmt=999) 2, LF3d_grid_x(2), 2, LF3d_grid_y(2), 2, LF3d_grid_z(2)
+  WRITE(*,*) '      ..   .......      ..   .......      ..   .......'
+  WRITE(*,fmt=999) Nx-1, LF3d_grid_x(Nx-1), Ny-1, LF3d_grid_y(Ny-1), Nz-1, LF3d_grid_z(Nz-1)
+  WRITE(*,fmt=999) Nx,   LF3d_grid_x(Nx), Ny, LF3d_grid_y(Ny), Nz, LF3d_grid_z(Nz)
 
-  ! y-direction --------------------------------------
-  N = LF3d_NN(2)
-  WRITE(*,'(/,1x,A)') 'Some grid points in y-direction:'
-  WRITE(*,*) 1, LF3d_grid_y(1)
-  WRITE(*,*) 2, LF3d_grid_y(2)
-  WRITE(*,*) '....'
-  WRITE(*,*) N-1, LF3d_grid_y(N-1)
-  WRITE(*,*) N,   LF3d_grid_y(N)
-
-  ! z-direction --------------------------------------
-  N = LF3d_NN(3)
-  WRITE(*,'(/,1x,A)') 'Some grid points in z-direction:'
-  WRITE(*,*) 1, LF3d_grid_z(1)
-  WRITE(*,*) 2, LF3d_grid_z(2)
-  WRITE(*,*) '....'
-  WRITE(*,*) N-1, LF3d_grid_z(N-1)
-  WRITE(*,*) N,   LF3d_grid_z(N)
+  999 FORMAT(1x,I8,F10.5,I8,F10.5,I8,F10.5)
 
   ! FIXME This should be called only for periodic case
   ! G2 ------------------------------------------
   N = LF3d_Npoints
-  WRITE(*,'(/,1x,A)') 'Some G2 values:'
-  WRITE(*,*) 1, LF3d_G2(1)
-  WRITE(*,*) 2, LF3d_G2(2)
-  WRITE(*,*) '....'
-  WRITE(*,*) N-1, LF3d_G2(N-1)
-  WRITE(*,*) N,   LF3d_G2(N)
+  WRITE(*,'(/,1x,A,/)') 'Some G2 values:'
+  WRITE(*,'(1x,I10,F10.5)') 1, LF3d_G2(1)
+  WRITE(*,'(1x,I10,F10.5)') 2, LF3d_G2(2)
+  WRITE(*,*) '       ...   .......'
+  WRITE(*,'(1x,I10,F10.5)') N-1, LF3d_G2(N-1)
+  WRITE(*,'(1x,I10,F10.5)') N,   LF3d_G2(N)
 
+  WRITE(*,*) '--------------------------------------------------------------'
+  WRITE(*,*)
+
+  STOP 'stopped'
 END SUBROUTINE
 
