1492af3853ccc97af76e73a52a4ea82da68a249e
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-03-24 19:47:28 +0700

    Initial test of PETSC to build Laplacian matrix

---
+++ tests/petsc/do_build_petsc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+INC="-I/home/efefer/mysoftwares/petsc-3.7.5/include/ -I../../"
+LIB="../../libmain.a -lblas -llapack -lfftw3 \
+-L/home/efefer/mysoftwares/petsc-3.7.5/lib -lpetsc"
+
+bas=`basename $1 .F`
+
+mpifort -O3 -Wall -ffree-form $INC $1 $LIB -o $bas.x
+
+# for
+#mpifort -free $INC $1 $LIB -o $bas.x
+
