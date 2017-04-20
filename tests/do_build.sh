369a39708ba8d8525d8eba4d8050803cbefcdae3
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-04-20 10:25:49 +0700

    Removing deprecated test_Emin_cg_H

--- tests/do_build.sh
+++ tests/do_build.sh
@@ -12,8 +12,10 @@ bas=`basename $1 .f90`
 # remove the previous executable
 rm -vf $bas.x
 
+$2 $INC $1 $LIB -o $bas.x
+
 #gfortran -Wall -O3 -ffree-form $INC $1 $LIB -o $bas.x
-#echo "Test executable: $bas.x"
+echo "Test executable: $bas.x"
 
 # for
 #mpifort -free $INC $1 $LIB -o $bas.x
