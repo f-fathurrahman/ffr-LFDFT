947e0c3a446831d9dc84e48d40a20bc698e6da1b
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-05-03 22:58:00 +0700

    Fix bug in calc_dr_periodic_1pnt

--- calc_dr_periodic_1pnt.f90
+++ calc_dr_periodic_1pnt.f90
@@ -31,8 +31,8 @@ SUBROUTINE calc_dr_periodic_1pnt( LL, r0, lingrid, dr_vec )
   Lz = LL(3)
 
   xx1 = lingrid(1) - r0(1)
-  xx2 = r0(1) + Lx - lingrid(1)
-  xx3 = lingrid(1) - r0(1) - Lx
+  xx2 = lingrid(1) + Lx - r0(1)
+  xx3 = lingrid(1) - Lx - r0(1)
   IF( abs(xx1) < abs(xx2) ) THEN
     dr_vec(1) = xx1
   ELSE
@@ -42,8 +42,8 @@ SUBROUTINE calc_dr_periodic_1pnt( LL, r0, lingrid, dr_vec )
 
   
   yy1 = lingrid(2) - r0(2)
-  yy2 = r0(2) + Ly - lingrid(2)
-  yy3 = lingrid(2) - r0(2) - Ly
+  yy2 = lingrid(2) + Ly - r0(2)
+  yy3 = lingrid(2) - Ly - r0(2)
   IF( abs(yy1) < abs(yy2) ) THEN
     dr_vec(2) = yy1
   ELSE
@@ -53,14 +53,17 @@ SUBROUTINE calc_dr_periodic_1pnt( LL, r0, lingrid, dr_vec )
 
   
   zz1 = lingrid(3) - r0(3)
-  zz2 = r0(3) + Lz - lingrid(3)
-  zz3 = lingrid(3) - r0(3) - Lz
+  zz2 = lingrid(3) + Lz - r0(3)
+  zz3 = lingrid(3) - Lz - r0(3)
   IF( abs(zz1) < abs(zz2) ) THEN
     dr_vec(3) = zz1
   ELSE
     dr_vec(3) = zz2
   ENDIF 
   IF( abs(dr_vec(3) ) > abs(zz3) ) dr_vec(3) = zz3
+
+  ! special case ??
+  !IF( lingrid(3) > Lz/2.d0 .AND. r0(3) == 0.d0 ) dr_vec(3) = -dr_vec(3)
 
 END SUBROUTINE
 
--- tests/test_Ylm_real.f90
+++ tests/test_Ylm_real.f90
@@ -1,6 +1,5 @@
 PROGRAM test_Ylm_real
 
-  USE m_PsPot, ONLY : PsPot_Dir
   USE m_LF3d, ONLY : lingrid => LF3d_lingrid, &
                      grid_x => LF3d_grid_x, &
                      grid_y => LF3d_grid_y, &
@@ -20,6 +19,9 @@ PROGRAM test_Ylm_real
   REAL(8) :: LatVecs(3,3), origin(3)
   REAL(8), ALLOCATABLE :: fun(:)
   REAL(8) :: Ylm_real
+  INTEGER :: l,m
+  INTEGER :: unitxsf
+  CHARACTER(32) :: filexsf
 
   Narg = iargc()
   IF( Narg /= 2 ) THEN 
@@ -37,10 +39,6 @@ PROGRAM test_Ylm_real
   ! so that coord given in xyz file is in bohr
   !atpos(:,:) = atpos(:,:)/ANG2BOHR  
 
-  ! Override PsPot_Dir
-  PsPot_Dir = '../HGH/'
-  CALL init_PsPot()
-
   !
   NN = (/ N_in, N_in, N_in /)
   AA = (/ 0.d0, 0.d0, 0.d0 /)
@@ -48,40 +46,39 @@ PROGRAM test_Ylm_real
   CALL init_LF3d_p( NN, AA, BB )
 
   CALL info_atoms()
-  CALL info_PsPot()
   CALL info_LF3d()
 
-  ! Initialize occupation numbers
-  CALL init_states()
-
-  CALL init_strfact_shifted()
-
-  CALL calc_Ewald()
-
-  CALL alloc_hamiltonian()
-
-  CALL init_V_ps_loc_G()
+  ALLOCATE( fun(Npoints) )
 
   LatVecs(:,:) = 0.d0
   LatVecs(1,1) = BB(1) - AA(1)
   LatVecs(2,2) = BB(2) - AA(2)
   LatVecs(3,3) = BB(3) - AA(3)
-  CALL xsf_struct( LatVecs, Natoms, atpos, SpeciesSymbols, atm2species, 444 )
+
+  unitxsf = 444
   origin(1) = 0.5d0*( grid_x(2) - grid_x(1) )
   origin(2) = 0.5d0*( grid_y(2) - grid_y(1) )
   origin(3) = 0.5d0*( grid_z(2) - grid_z(1) )
-  !CALL xsf_fast_datagrid_3d(V_ps_loc, NN(1), NN(2), NN(3), NN(1), NN(2), NN(3), &
-  !          origin, LatVecs, 444)
+  ia = 1  ! always use the first atom as the center
 
-  ALLOCATE( fun(Npoints) )
-  ia = 1
-  DO ip = 1,Npoints
-    CALL calc_dr_periodic_1pnt( LL, atpos(:,ia), lingrid(:,ip), dr_vec )
-    dr = sqrt( dr_vec(1)**2 + dr_vec(2)**2 + dr_vec(3)**2 )
-    fun(ip) = exp(-0.5d0*dr) * Ylm_real( 3,1, dr_vec )
+  DO l = 0,3
+    
+    m = 0
+    WRITE( filexsf, '(A,I1,A)' ) 'fort.',l,'.0'
+    WRITE(*,*) 'filexsf = ', filexsf
+    OPEN( unit=unitxsf, file=filexsf )
+    CALL xsf_struct( LatVecs, Natoms, atpos, SpeciesSymbols, atm2species, unitxsf )
+    DO ip = 1,Npoints
+      CALL calc_dr_periodic_1pnt( LL, atpos(:,ia), lingrid(:,ip), dr_vec )
+      dr = sqrt( dr_vec(1)**2 + dr_vec(2)**2 + dr_vec(3)**2 )
+      !WRITE(*,'(1x,6F8.3)') lingrid(:,ip), dr_vec(1:3)
+      fun(ip) = exp(-0.5d0*dr) * Ylm_real( l,m, dr_vec )
+    ENDDO 
+    CALL xsf_fast_datagrid_3d(fun, NN(1), NN(2), NN(3), NN(1), NN(2), NN(3), &
+            origin, LatVecs, unitxsf)
+    CLOSE(unitxsf)
+
   ENDDO 
-  CALL xsf_fast_datagrid_3d(fun, NN(1), NN(2), NN(3), NN(1), NN(2), NN(3), &
-            origin, LatVecs, 444)
 
 
   DEALLOCATE( fun )
