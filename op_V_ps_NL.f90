f6daab9cb5e607470d453f821b0637031fc3df8e
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-05-22 21:18:29 +0700

    Implemented multicolumn op_V_ps_NL, testing SCF for NL pseudopot

--- op_V_ps_NL.f90
+++ op_V_ps_NL.f90
@@ -1,12 +1,14 @@
 SUBROUTINE op_V_ps_NL( Nstates, Vpsi )
   USE m_LF3d, ONLY : Npoints => LF3d_Npoints
-  USE m_PsPot, ONLY : NbetaNL, betaNL, w_NL
-  USE m_atoms, ONLY : Natoms
+  USE m_PsPot, ONLY : NbetaNL, betaNL, prj2beta, Ps => Ps_HGH_Params
+  USE m_atoms, ONLY : Natoms, atm2species
   USE m_hamiltonian, ONLY : betaNL_psi
   IMPLICIT NONE 
   INTEGER :: Nstates
   REAL(8) :: Vpsi(Npoints,Nstates)
-  INTEGER :: ia, ibeta, ist
+  INTEGER :: ia, isp, ist, ibeta, jbeta, iprj, jprj
+  INTEGER :: l, m
+  REAL(8) :: hij
 
   IF( NbetaNL <= 0 ) THEN 
     RETURN 
@@ -16,9 +18,19 @@ SUBROUTINE op_V_ps_NL( Nstates, Vpsi )
 
   DO ist = 1,Nstates
     DO ia = 1,Natoms
-      DO ibeta = 1,NbetaNL
-        Vpsi(:,ist) = w_NL(ibeta)*betaNL(:,ibeta)*betaNL_psi(ia,ist,ibeta)
-      ENDDO 
+      isp = atm2species(ia)
+      DO l = 0,Ps(isp)%lmax
+      DO m = -l,l
+        DO iprj = 1,Ps(isp)%Nproj_l(l)
+        DO jprj = 1,Ps(isp)%Nproj_l(l)
+          ibeta = prj2beta(iprj,ia,l,m)
+          jbeta = prj2beta(jprj,ia,l,m)
+          hij = Ps(isp)%h(l,iprj,jprj)
+          Vpsi(:,ist) = Vpsi(:,ist) + hij*betaNL(:,ibeta)*betaNL_psi(ia,ist,jbeta)
+        ENDDO ! jprj
+        ENDDO ! iprj
+      ENDDO ! m
+      ENDDO ! l
     ENDDO 
   ENDDO 
 
