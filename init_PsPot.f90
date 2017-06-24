413958a527a6100ff8b882fd1dd9c493494f46d2
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-06-24 17:04:59 +0700

    Some cleanup in init_PsPot

--- init_PsPot.f90
+++ init_PsPot.f90
@@ -7,21 +7,19 @@ SUBROUTINE init_PsPot()
   USE m_Ps_HGH, ONLY : init_Ps_HGH_Params
 
   IMPLICIT NONE 
-  INTEGER :: isp, iprj, l, ia, m, ibeta
+  INTEGER :: isp, iprj, l, ia, m
 
   ! Use default
   ALLOCATE( PsPot_FilePath(Nspecies) )
   ALLOCATE( Ps_HGH_Params(Nspecies) )
 
   DO isp = 1,Nspecies
-
+    ! initialize HGH pseudopotentials for each atomic species
     PsPot_FilePath(isp) = trim(PsPot_Dir) // trim(SpeciesSymbols(isp)) // '.hgh'
     CALL init_Ps_HGH_Params( Ps_HGH_Params(isp), PsPot_FilePath(isp) )
-    
+    ! Set atomic valences
     AtomicValences(isp) = Ps_HGH_Params(isp)%zval
-
   ENDDO 
-
 
   ALLOCATE( prj2beta(1:3,1:Natoms,0:3,-3:3) )
   prj2beta(:,:,:,:) = -1
@@ -29,13 +27,10 @@ SUBROUTINE init_PsPot()
   DO ia = 1,Natoms
     isp = atm2species(ia)
     DO l = 0,Ps_HGH_Params(isp)%lmax
-!      WRITE(*,*)
       DO iprj = 1,Ps_HGH_Params(isp)%Nproj_l(l)
         DO m = -l,l
           NbetaNL = NbetaNL + 1
           prj2beta(iprj,ia,l,m) = NbetaNL
-!          WRITE(*,*) NbetaNL, ia, '(',l, ',', m, ')', iprj, &
-!                     prj2beta(iprj,ia,l,m), Ps_HGH_Params(isp)%h(l,iprj,iprj)                     
         ENDDO ! m
       ENDDO ! iprj
     ENDDO ! l
