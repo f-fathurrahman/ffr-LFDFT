d8a7f870183bcc17c136eb0e882a87f545fca782
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-07-20 22:05:28 +0700

    Add example calc to investigate eggbox effect

---
+++ works/eggbox_atom_H/along_diag/do_run.py
@@ -0,0 +1,24 @@
+import numpy as np
+import os
+
+start_pos = np.array([0.0, 0.0, 0.0])
+dx = 0.02
+Nmoves = 20
+
+f = open('TEMPLATE_INP', 'r')
+lines = f.readlines()
+f.close()
+
+pos = start_pos[:]
+for i in range(Nmoves):
+    #
+    infile  = 'INPUT_' + str(i+1)
+    outfile = 'LOG_' + str(i+1)
+    #
+    f = open(infile, 'w')
+    f.writelines(lines)
+    pos[:] = pos[:] + i*dx
+    f.write('H  %18.10f %18.10f %18.10f\n' % (pos[0],pos[1],pos[2]))
+    f.close()
+    #
+    os.system('../../../ffr_LFDFT_gfortran.x ' + infile + ' | tee ' + outfile)
