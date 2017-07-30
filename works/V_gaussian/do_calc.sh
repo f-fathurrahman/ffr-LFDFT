b25a4d49aaabc122dd0859a3b0a4181cb01b553c
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-07-30 14:08:58 +0700

    Collect several convergence data for Gaussian potential

--- works/V_gaussian/do_calc.sh
+++ works/V_gaussian/do_calc.sh
@@ -1,6 +1,7 @@
-for nn in 11 13 15 17 19 21 23 27 33 35
+#/bin/bash
+for nn in 11 13 15 17 19 21 23 27 33 35 39 41 45 49 55
 do
-  ./do_Emin_pcg_gaussian_G.x $nn $1 $2 > fort.log.$nn
+  ./do_Emin_pcg_gaussian_G.x $nn $1 $2 | tee fort.log.$nn
 done
 
 grep "Total    =" fort.log.* > RESULT.dat
