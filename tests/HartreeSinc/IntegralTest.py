b51988e915c22181af9dda1b0cd77d6b023aef10
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-08-05 23:22:26 +0700

    Test diagonal element of matrix F

--- tests/HartreeSinc/IntegralTest.py
+++ tests/HartreeSinc/IntegralTest.py
@@ -6,11 +6,11 @@ x = symbols('x')
 
 PI = pi.n()
 # basis function
-x_alpha = 0.4
+x_alpha = 0.2
 u = sin(PI/h*(x-x_alpha))/sqrt(h)/(PI*(x-x_alpha)/h)
 print(u)
 
-t = 0.0
+t = 0.1
 f = exp(-t**2*(x-x_alpha)**2) * u
 print(f)
 
