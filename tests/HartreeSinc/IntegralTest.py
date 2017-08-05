2092945deae97f134ca2daa4dc3c8b1ac7a2a5e5
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-08-05 12:25:33 +0700

    Add IntegralTest

---
+++ tests/HartreeSinc/IntegralTest.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+from sympy import *
+
+h = 0.2
+x = symbols('x')
+
+PI = pi.n()
+# basis function
+x_alpha = 0.4
+u = sin(PI/h*(x-x_alpha))/sqrt(h)/(PI*(x-x_alpha)/h)
+print(u)
+
+t = 0.0
+f = exp(-t**2*(x-x_alpha)**2) * u
+print(f)
+
+print( Integral( f, (x,-oo,oo) ).evalf() )
