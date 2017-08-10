679535cd8248e29345b7a9b5dddddd2e7e1922f3
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-08-10 14:59:25 +0700

    Collect data for LF sinc convergence for Gaussian potential

--- works/V_gaussian_sinc/do_calc.sh
+++ works/V_gaussian_sinc/do_calc.sh
@@ -1,8 +1,7 @@
 #/bin/bash
-#for nn in 11 13 15 17 19 21 23 27 33 35 39 41 45 49 55
 #for nn in 10 12 14 16 18 20 22 26 32 34 38 40 44 48 54
 #do
-  #./do_Emin_pcg_gaussian_sinc.x $nn $1 $2 | tee fort.log.$nn
+#  ./do_Emin_pcg_gaussian_sinc.x $nn $1 $2 | tee fort.log.$nn
 #done
 
 grep "! Elec" fort.log.* > RESULT.dat
