37d26e51509c39766cd5fe0cf6bdb066d80dc9a4
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-08-19 22:51:27 +0700

    Enable LaTeX mode

--- tests/init/doPlot.py
+++ tests/init/doPlot.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 from matplotlib import rc
 rc('font',**{'family':'serif', 'size':16})
@@ -14,4 +15,8 @@ for t in types:
     for ibf in range(1,NBASIS+1):
         plt.plot( dat1[:,0], dat1[:,ibf] )
     plt.xlim( dat1[-1,0], dat1[0,0] )
-    plt.savefig('LF1d_' + t + '.pdf')
+    plt.grid()
+    FIPLOT = 'LF1d_' + t + '.pdf'
+    plt.savefig(FIPLOT)
+    #
+    os.system('pdfcrop ' + FIPLOT + ' ' + FIPLOT)
