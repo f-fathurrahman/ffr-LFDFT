8b0b3b024e0559df259f7d12588b36e7a73a8c3b
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-08-26 20:43:04 +0700

    Test integral in 1d

--- tests/integ_1d/integ_1d.f90
+++ tests/integ_1d/integ_1d.f90
@@ -1,7 +1,68 @@
 ! test convergence of integrals
 ! TODO: also test egg-box effect in 1d ???
+
+! A periodic function
+FUNCTION funcx( c1, L, x )
+  USE m_constants, ONLY : PI
+  IMPLICIT NONE
+  !
+  REAL(8) :: funcx
+  REAL(8) :: c1, L, x
+
+  funcx = exp( 1.5*cos(2.d0*PI/L *(x - c1) ) )
+END FUNCTION
+
+
+
 PROGRAM test_integral
+  USE m_constants, ONLY : PI
+  USE m_LF3d, ONLY : grid_x => LF3d_grid_x
+  IMPLICIT NONE
+  !
+  INTEGER :: N
+  REAL(8) :: L
+  !
+  REAL(8), ALLOCATABLE :: coefs(:)
+  !
+  INTEGER :: ii, jj
+  ! Functions
+  REAL(8) :: funcx
+  !
+  INTEGER :: NPTS_PLOT=401
+  REAL(8) :: xx, yy, h, c1
+  !
+  REAL(8) :: eval_LF1d_p
+  !
+  CHARACTER(8) :: chars_N
+  INTEGER :: iargc
+  
+  IF( iargc() /= 1 ) THEN 
+    WRITE(*,*) 'ERROR: exactly one argument is needed'
+    STOP 
+  ENDIF 
 
+  CALL getarg( 1, chars_N )
+  READ( chars_N, * ) N
 
+  L = 10.d0
+
+  c1 = L/2.d0  ! position
+
+  ! Initialize the basis functions
+  ALLOCATE( grid_x(N) )
+  CALL init_grid_1d_p( N, 0.d0, L, grid_x )
+
+  h = L/dble(N)  ! manually calculate h
+
+  ! Calculate coefficients of expansion for the function
+  ALLOCATE( coefs(N) )
+
+  DO ii=1,N
+    coefs(ii) = sqrt(h)* funcx( c1, L, grid_x(ii) ) 
+    WRITE(11,'(1x,2F18.10)') grid_x(ii), coefs(ii)/sqrt(h)
+  ENDDO
+
+  WRITE(*,*) 'Integ = ', sum(coefs)*h
+  
 END PROGRAM 
 
