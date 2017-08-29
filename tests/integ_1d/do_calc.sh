329910d022452b3603454b966aa45aa0db38f480
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-08-29 21:13:34 +0700

    Reverse the order of iteration

--- tests/integ_1d/do_calc.sh
+++ tests/integ_1d/do_calc.sh
@@ -1,6 +1,7 @@
-for scal in `seq 0.0 0.1 0.9`; do
-  for N in `seq 3 2 55`; do
-    ./integ_1d.x $N $scal 1.0 2.5
+for N in `seq 3 2 55`; do
+  for scal in `seq 0.0 0.1 0.9`; do
+    ./integ_1d.x $N $scal 1.0 3.5
   done
+  echo "#"
 done
 
