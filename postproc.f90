f748adf7d138839a2526c710561aadebcf27f2cf
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-09-20 20:44:03 +0700

    Test to output all Nstates wavefunctions in XSF

--- postproc.f90
+++ postproc.f90
@@ -10,16 +10,24 @@ END PROGRAM
 SUBROUTINE write_KS_evecs_xsf()
   USE m_checkpoint
   USE m_io_data
-  USE m_LF3d, ONLY : Npoints => LF3d_Npoints
   USE m_states, ONLY : KS_evecs, Nstates
   IMPLICIT NONE 
+  INTEGER :: ist
+  CHARACTER(56) :: filename, str_ist
 
   CALL read_checkpoint()
   CALL read_KS_evecs('KS_evecs.dat')
 
-  WRITE(*,*) 'Npoints = ', Npoints
-  WRITE(*,*) 'Nstates = ', Nstates
-
-  CALL write_data3d_xsf(KS_evecs(:,1), 'evecs.xsf')
+  DO ist = 1, Nstates
+    IF( ist < 10 ) THEN 
+      WRITE(str_ist,'(I1)') ist
+    ELSEIF( ist < 100 ) THEN 
+      WRITE(str_ist,'(I2)') ist
+    ELSEIF( ist < 1000 ) THEN 
+      WRITE(str_ist,'(I3)') ist
+    ENDIF 
+    filename = 'evecs_'//trim(str_ist)//'.xsf'
+    CALL write_data3d_xsf(KS_evecs(:,ist), filename)
+  ENDDO
 END SUBROUTINE 
 
