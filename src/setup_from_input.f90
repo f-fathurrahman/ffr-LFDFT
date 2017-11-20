131d9527ed5132704f0eb0bd3ddab1e8edf71d34
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-11-20 19:32:41 +0700

    Change the subroutine calls order in setup_from_input

--- src/setup_from_input.f90
+++ src/setup_from_input.f90
@@ -1,10 +1,9 @@
 SUBROUTINE setup_from_input()
   USE m_input_vars
-  IMPLICIT NONE 
+  IMPLICIT NONE
 
   CALL setup_atoms()
-  CALL setup_PsPot()
   CALL setup_LF3d()
+  CALL setup_PsPot()
 
-END SUBROUTINE 
-
+END SUBROUTINE
