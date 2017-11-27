2dc6f9972fd133642ff2584c193fad164853a126
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-11-27 17:13:23 +0700

    Add comment as reminder to change variable names (to be put into consideration)

--- src/m_atoms.f90
+++ src/m_atoms.f90
@@ -7,8 +7,8 @@ MODULE m_atoms
   REAL(8), ALLOCATABLE :: AtomicCoords(:,:)
   INTEGER, ALLOCATABLE :: atm2species(:)
   CHARACTER(5), ALLOCATABLE :: SpeciesSymbols(:)  ! Nspecies
-  REAL(8), ALLOCATABLE :: AtomicValences(:)
-  REAL(8), ALLOCATABLE :: AtomicMasses(:)
+  REAL(8), ALLOCATABLE :: AtomicValences(:)  ! should be SpeciesValences
+  REAL(8), ALLOCATABLE :: AtomicMasses(:)    ! should be SpeciesMasses
 
   COMPLEX(8), ALLOCATABLE :: StructureFactor(:,:)
 
