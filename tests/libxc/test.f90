242efe3aa090fc15df4f47c53e560c9d6c49746d
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-12-07 18:45:37 +0700

    Test calling exc_vxc of LibXC

--- tests/libxc/test.f90
+++ tests/libxc/test.f90
@@ -1,33 +1,45 @@
-program lxctest
-   use xc_f90_types_m
-   use xc_f90_lib_m
+PROGRAM lxctest
+   USE xc_f90_types_m
+   USE xc_f90_lib_m
 
-   implicit none
+   IMPLICIT NONE 
 
    TYPE(xc_f90_pointer_t) :: xc_func
    TYPE(xc_f90_pointer_t) :: xc_info
-   real(8) :: rho(5) = (/0.1, 0.2, 0.3, 0.4, 0.5/)
-   real(8) :: sigma(5) = (/0.2, 0.3, 0.4, 0.5, 0.6/)
-   real(8) :: exc(5)
-   integer :: i, vmajor, vminor, vmicro, func_id = 1
+   REAL(8) :: rho(5) = (/0.1d0, 0.2d0, 0.3d0, 0.4d0, 0.5d0/)
+   REAL(8) :: sigma(5) = (/0.2d0, 0.3d0, 0.4d0, 0.5d0, 0.6d0/)
+   !
+   REAL(8) :: vrho(5)
+   REAL(8) :: vsigma(5)
+   REAL(8) :: exc(5)
+   !
+   INTEGER :: i, vmajor, vminor, vmicro
+   INTEGER :: func_id = 1  ! LDA
+   !INTEGER :: func_id = 101  ! PBE
 
-   call xc_f90_version(vmajor, vminor, vmicro)
-   write(*,'("Libxc version: ",I1,".",I1,".",I1)') vmajor, vminor, vmicro
+   CALL xc_f90_version(vmajor, vminor, vmicro)
+   WRITE(*,'("Libxc version: ",I1,".",I1,".",I1)') vmajor, vminor, vmicro
 
-   call xc_f90_func_init(xc_func, xc_info, func_id, XC_UNPOLARIZED)
+   CALL xc_f90_func_init(xc_func, xc_info, func_id, XC_UNPOLARIZED)
 
-   select case (xc_f90_info_family(xc_info))
-   case(XC_FAMILY_LDA)
-     call xc_f90_lda_exc(xc_func, 5, rho(1), exc(1))
-   case(XC_FAMILY_GGA, XC_FAMILY_HYB_GGA)
-     call xc_f90_gga_exc(xc_func, 5, rho(1), sigma(1), exc(1))
-   end select
+   SELECT CASE (xc_f90_info_family(xc_info))
+   CASE(XC_FAMILY_LDA)
+     WRITE(*,*) 'XC_FAMILY_LDA'
+     !CALL xc_f90_lda_exc(xc_func, 5, rho(1), exc(1))
+     CALL xc_f90_lda_exc_vxc(xc_func, 5, rho(1), exc(1), vrho(1))  
+   CASE(XC_FAMILY_GGA, XC_FAMILY_HYB_GGA)
+     WRITE(*,*) 'XC_FAMILY_GGA'
+     !CALL xc_f90_gga_exc(xc_func, 5, rho(1), sigma(1), exc(1))
+     CALL xc_f90_gga_exc_vxc(xc_func, 5, rho(1), sigma(1), exc(1), vrho(1), vsigma(1))
+   END SELECT 
 
-   do i = 1, 5
-     write(*,"(F8.6,1X,F9.6)") rho(i), exc(i)
-   end do
+   DO i = 1, 5
+     WRITE(*,'(1x,5F18.10)') rho(i), sigma(i), exc(i), vrho(i), vsigma(i)
+   ENDDO
 
-   call xc_f90_func_end(xc_func)
+   CALL xc_f90_func_end(xc_func)
 
-end program lxctest
+END PROGRAM 
+
+
 
