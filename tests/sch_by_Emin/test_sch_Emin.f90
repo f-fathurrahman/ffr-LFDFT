146768c5868c9eebab419a99cd00a77c37fe6ede
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-01-24 16:54:31 +0700

    Test Davidson, LOBPCG, and Emin

--- tests/sch_by_Emin/test_sch_Emin.f90
+++ tests/sch_by_Emin/test_sch_Emin.f90
@@ -1,21 +1,20 @@
 PROGRAM test_sch
 
   USE m_constants, ONLY: PI
-  USE m_LF3d, ONLY : Npoints => LF3d_Npoints, &
-                     dVol => LF3d_dVol
+  USE m_LF3d, ONLY : Npoints => LF3d_Npoints, dVol => LF3d_dVol
   USE m_states, ONLY : Nstates, &
                        Focc, &
                        evals => KS_evals, &
                        evecs => KS_evecs
   USE m_hamiltonian, ONLY : V_ps_loc
+  USE m_PsPot, ONLY : NbetaNL
   IMPLICIT NONE
   !
   INTEGER :: ist, ip
   INTEGER :: NN(3)
   REAL(8) :: AA(3), BB(3)
-  !
   REAL(8) :: ddot
-  
+  !
   NN = (/ 35, 35, 35 /)
   AA = (/ 0.d0, 0.d0, 0.d0 /)
   BB = (/ 6.d0, 6.d0, 6.d0 /)
@@ -31,31 +30,31 @@ PROGRAM test_sch
   CALL init_ilu0_prec()
 
   CALL init_V_ps_loc_harmonic( 2.d0, 0.5*(BB-AA) )
+  WRITE(*,*)
   WRITE(*,*) 'sum(V_ps_loc) = ', sum(V_ps_loc)
 
   Nstates = 4
   ALLOCATE( Focc(Nstates) )
   Focc(:) = 1.d0
 
-  ALLOCATE( evecs(Npoints,Nstates), evals(Nstates) )
+  NbetaNL = 0
 
+  ! Set up initial guess
+  ALLOCATE( evecs(Npoints,Nstates), evals(Nstates) )
   DO ist = 1, Nstates
     DO ip = 1, Npoints
       CALL random_number( evecs(ip,ist) )
     ENDDO
   ENDDO
-  CALL ortho_gram_schmidt( evecs, Npoints, Npoints, Nstates )
+  CALL orthonormalize( Nstates, evecs )
 
-  CALL diag_lobpcg( evals, evecs, 1.0d-4, .TRUE. )
+  CALL Sch_solve_Emin_pcg( 3.d-5, .FALSE., 1d-4, 100 )
 
   WRITE(*,*)
   WRITE(*,*) 'Final eigenvalues:'
   DO ist = 1, Nstates
     WRITE(*,'(1x,I4,F18.10)') ist, evals(ist)
   ENDDO
-
-  ! remember to renormalize eigenvectors
-  evecs(:,:) = evecs(:,:)/sqrt(dVol)
 
   WRITE(*,*)
   WRITE(*,*) 'Check normalization:'
--- tests/sch_by_diag/test_sch_davidson.f90
+++ tests/sch_by_diag/test_sch_davidson.f90
@@ -1,19 +1,21 @@
 PROGRAM test_sch
 
   USE m_constants, ONLY: PI
-  USE m_LF3d, ONLY : Npoints => LF3d_Npoints
+  USE m_LF3d, ONLY : Npoints => LF3d_Npoints, &
+                     dVol => LF3d_dVol
   USE m_states, ONLY : Nstates, &
                        Focc, &
                        evals => KS_evals, &
                        evecs => KS_evecs
   USE m_hamiltonian, ONLY : V_ps_loc
-  USE m_PsPot, ONLY : NbetaNL
   IMPLICIT NONE
   !
   INTEGER :: ist, ip
   INTEGER :: NN(3)
   REAL(8) :: AA(3), BB(3)
   !
+  REAL(8) :: ddot
+  
   NN = (/ 35, 35, 35 /)
   AA = (/ 0.d0, 0.d0, 0.d0 /)
   BB = (/ 6.d0, 6.d0, 6.d0 /)
@@ -29,25 +31,35 @@ PROGRAM test_sch
   CALL init_ilu0_prec()
 
   CALL init_V_ps_loc_harmonic( 2.d0, 0.5*(BB-AA) )
-  WRITE(*,*)
   WRITE(*,*) 'sum(V_ps_loc) = ', sum(V_ps_loc)
 
   Nstates = 4
   ALLOCATE( Focc(Nstates) )
   Focc(:) = 1.d0
 
-  NbetaNL = 0
-
-  ! Set up initial guess
   ALLOCATE( evecs(Npoints,Nstates), evals(Nstates) )
+
   DO ist = 1, Nstates
     DO ip = 1, Npoints
       CALL random_number( evecs(ip,ist) )
     ENDDO
   ENDDO
+  ! Note the difference in normalization convention
   CALL orthonormalize( Nstates, evecs )
 
-  CALL Sch_solve_Emin_pcg( 3.d-5, .FALSE., 1d-4, 100 )
+  CALL diag_davidson( evals, evecs, 1.0d-4, .TRUE. )
+
+  WRITE(*,*)
+  WRITE(*,*) 'Final eigenvalues:'
+  DO ist = 1, Nstates
+    WRITE(*,'(1x,I4,F18.10)') ist, evals(ist)
+  ENDDO
+
+  WRITE(*,*)
+  WRITE(*,*) 'Check normalization:'
+  DO ist = 1, Nstates
+    WRITE(*,'(1x,I4,F18.10)') ist, ddot( Npoints, evecs(:,ist), 1, evecs(:,ist), 1 )*dVol
+  ENDDO 
 
   DEALLOCATE( evecs, evals )
   DEALLOCATE( Focc )
