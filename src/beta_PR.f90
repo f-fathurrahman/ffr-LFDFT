f710ced2992b09d49bd1d009ab0b442110084c40
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-01-25 22:49:39 +0700

    Use ddot in beta_PR

--- src/beta_PR.f90
+++ src/beta_PR.f90
@@ -5,12 +5,12 @@ FUNCTION beta_PR( N, g, g_old, Kg, Kg_old ) RESULT(beta)
   REAL(8) :: num, denum, beta
   !
   INTEGER :: i
+  REAL(8) :: ddot
   
   num = 0.d0
-  denum = 0.d0
   DO i = 1,N
     num = num + ( g(i) - g_old(i) ) * Kg(i)
-    denum = denum + g_old(i) * Kg_old(i)
   ENDDO 
+  denum = ddot( N, g_old, 1, Kg_old, 1 )
   beta = num/denum
 END FUNCTION 
