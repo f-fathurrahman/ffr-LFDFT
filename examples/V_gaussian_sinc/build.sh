8053f7dfc0d409a09c6851cc4de9a440a671e5da
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-06-08 20:54:16 +0700

    New build script

--- examples/V_gaussian_sinc/build.sh
+++ examples/V_gaussian_sinc/build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-INC="-I../../"
-LIB="../../libmain.a -lblas -llapack -lfftw3"
+if [ "$#" -ne 2 ]; then
+  echo
+  echo "ERROR"
+  echo "Need two parameters: main file and compiler name (and or options)"
+  echo "Example: ./build.sh myfile.f90 \"pgf90 -O2\""
+  echo
+  exit 1
+fi
+
+INC="-I../../src/"
+LIB="../../src/libmain.a ../../src/libsparskit.a ../../src/libpoisson_ISF.a \
+-L/home/efefer/mysoftwares/libxc-3.0.0/lib -lxcf90 -lxc \
+-lblas -llapack -lfftw3"
 
 bas=`basename $1 .f90`
 
@@ -10,5 +21,6 @@ rm -vf $bas.x
 
 $2 $INC $1 $LIB -o $bas.x
 
+#gfortran -Wall -O3 -ffree-form $INC $1 $LIB -o $bas.x
 echo "Test executable: $bas.x"
 
