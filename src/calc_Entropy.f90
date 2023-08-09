65a06c587a42160af74b3067197184b7660d937c
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-08-09 21:28:55 +0700

    Rename

--- src/calc_Entropy.f90
+++ src/calc_Entropy.f90
@@ -1,34 +1,28 @@
-! Calculate electronic entropy
-SUBROUTINE calc_Entropy( is_spinpol, Nstates, swidth, Focc, Entropy )
+FUNCTION calc_entropy( Nstates, Focc, Tbeta ) RESULT(e)
+  USE m_constants, ONLY : EPS_SMALL
   IMPLICIT NONE 
-  LOGICAL :: is_spinpol
   INTEGER :: Nstates
-  REAL(8) :: swidth
   REAL(8) :: Focc(Nstates)
-  REAL(8) :: Entropy
+  REAL(8) :: Tbeta
   INTEGER :: ist
-  REAL(8), PARAMETER :: SMALL_OCC = 1.e-10
   REAL(8) :: e
 
-  !
-  ! FIXME: No check on value of Focc
-  !
-
+  ! nonzero occupations
+  !inz = find( occ > EPS_SMALL )
+  !occnz = occ(inz)
+  !e = sum( occnz*log(occnz) )
   e = 0.d0
-  DO ist = 1, Nstates
-    IF( Focc(ist) > SMALL_OCC ) THEN 
+  DO ist = 1,Nstates
+    ! XXX: probably need to use abs(Focc) here to anticipate negative occupation numbers ?
+    IF( Focc(ist) > EPS_SMALL ) THEN 
+      ! non-zero occupation
       e = e + Focc(ist)*log(Focc(ist))
-    ELSE
-      IF( is_spinpol ) THEN 
-        e = e + (1.d0 - Focc(ist))*log(1.d0 - Focc(ist))
-      ELSE
-        ! spin-degenerate case
-        e = e + 2.d0*(1.d0 - 0.5d0*Focc(ist))*log(1.d0 - 0.5d0*Focc(ist))
-      ENDIF 
+    ELSE 
+      ! zero occupation
+      e = e + (1.d0 - Focc(ist)) * log(1.d0 - Focc(ist))
     ENDIF 
-  ENDDO
-  
-  Entropy = -e*2.d0*swidth
+  ENDDO 
 
-END SUBROUTINE 
+  e = e / Tbeta
+END FUNCTION 
 
